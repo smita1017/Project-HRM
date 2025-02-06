b6d2c5aad08938e65b3255033624c7d8a3f44dbb
smita1017 <smita126@outlook.com>
2025-02-06 09:19:50 +0530

    This is sign up page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    theme: {
+      extend: {
+        fontFamily: {
+          montserrat: ["Montserrat", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
